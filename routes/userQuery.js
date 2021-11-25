e1ae4fc3cdb8cd122403bde80e86c623e8a9163d
Manali Shetye <manalis826@gmail.com>
2021-11-25 10:34:00 +0530

    initial commit

---
+++ routes/userQuery.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+const userController = require('../controller/userQueryController');
+
+
+router.get("/users", userController.user_get);
+router.post("/add", userController.user_post);
+router.patch("/update/:id", userController.user_patch_all);
+router.delete("/delete/:id", userController.user_delete);
+
+
+module.exports = router;
