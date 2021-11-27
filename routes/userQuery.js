90ae39511bcaca62d0aeaa18f67e96dd3a4fc792
Manali Shetye <manalis826@gmail.com>
2021-11-27 22:00:57 +0530

    added a atpatch route for a specific user update

--- routes/userQuery.js
+++ routes/userQuery.js
@@ -5,7 +5,7 @@ const userController = require('../controller/userQueryController');
 router.get("/users", userController.user_get);
 router.post("/add", userController.user_post);
 router.patch("/update/:id", userController.user_patch_all);
-// router.patch("/update/:id", userController.user_patch);
+router.patch("/update/:id", userController.user_patch);
 router.delete("/delete/:id", userController.user_delete);
 
 
